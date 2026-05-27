4176f4d52497981c2e94fca8c25db05a7aff5bec
HackerEdition <nicatmmmdov452@gmail.com>
2026-05-27 21:16:01 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,37 +1,35 @@
-from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import Application, CommandHandler, CallbackQueryHandler, ContextTypes
 import os
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Application, CommandHandler, ContextTypes
 
 TOKEN = os.getenv("TOKEN")
+
+DIRECT_LINK = "https://t.me/ADSRBX_bot/ADSRBX"
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     keyboard = [
-        [InlineKeyboardButton("🚀 Başla", callback_data="go")]
+        [
+            InlineKeyboardButton(
+                "🚀 Başla",
+                url=DIRECT_LINK
+            )
+        ]
     ]
 
     await update.message.reply_text(
-        "👋 Başlamaq üçün bas",
+        "👋 Salam!\n\nBaşlamaq üçün düyməyə bas 👇",
         reply_markup=InlineKeyboardMarkup(keyboard)
     )
-
-
-async def button(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    query = update.callback_query
-    await query.answer()
-
-    if query.data == "go":
-        # heç mesaj göstərmir → sadəcə start olur
-        await query.delete_message()
 
 
 def main():
     app = Application.builder().token(TOKEN).build()
 
     app.add_handler(CommandHandler("start", start))
-    app.add_handler(CallbackQueryHandler(button))
 
+    print("Bot işləyir...")
     app.run_polling()
 
 
